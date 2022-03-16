713c94c49c56a0824abc846b5b1455f663a88566
mthiagoalves <m.thiago.alves@live.com>
2022-03-15 22:30:50 -0300

    andamento

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
 
+const pokedex = [
+  {
+    id: 001,
+    nome: "Bulbasaur",
+    descricao:
+      "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
+    tipo: "Grass",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png",
+  },
+  {
+    id: 004,
+    nome: "Charmander ",
+    descricao:
+      "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
+    tipo: "Fire",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+  },
+  {
+    id: 004,
+    nome: "Squirtle  ",
+    descricao:
+      "When it retracts its long neck into its shell, it squirts out water with vigorous force.",
+    tipo: "Water",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+  },
+];
 
-app.get('/', (req, res) => {
-  res.render("index");
-})
+app.get("/", (req, res) => {
+  res.render("index", {pokedex});
+});
 
-
-app.listen(5045, () => console.log(`Servidor rodando em http://localhost:5045`))
+app.listen(5045, () =>
+  console.log(`Servidor rodando em http://localhost:5045`)
+);
